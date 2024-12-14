a4622572375548ad148e0b4c53007db530cff866
Rodney Woollett <rodney.woollett@gmail.com>
2024-12-14 23:02:25 +1300

    Added acquireCS to client token component

--- front-end/src/components/dashboard/ClientToken.tsx
+++ front-end/src/components/dashboard/ClientToken.tsx
@@ -1,14 +1,20 @@
-import React, { useEffect, useState } from "react";
-import _, { findLastIndex } from 'lodash';
+import React, { useState } from "react";
+import _ from 'lodash';
 import { ActionByIp, TokenAction } from "../../types";
 import { format, parseISO } from "date-fns";
-import { Client, RequestCs, RequestedCsTokenSubscription, RequestedCsTokenSubscriptionVariables, useRequestedCsTokenSubscription } from "../../graphql/generated/graphql-cstoken";
-import { gql, TypedDocumentNode, useSubscription } from "@apollo/client";
+import {
+  Client, RequestCs, AcquireCs,
+  AcquiredCsTokenSubscription,
+  AcquiredCsTokenSubscriptionVariables,
+  RequestedCsTokenSubscription,
+  RequestedCsTokenSubscriptionVariables
+} from "../../graphql/generated/graphql-cstoken";
+import { gql, useSubscription, TypedDocumentNode } from "@apollo/client";
 import styles from './ClientToken.module.css'
 
 /**
  * Client Token activity on CSToken Network.
- * Show all network activity for clients connected  in the range of ports on network
+ * Show all network activity for clients connected in the range of ports on network
  * 
  */
 type ClientTokenProps = {
@@ -19,35 +25,31 @@ type ClientTokenProps = {
   clientsByIp: ActionByIp;
 }
 
-
-// const { data, error, loading } = useSubscription(
-//   query,
-//   {
-//     onData({ data }) {
-//       setAccumulatedData((prev) => [...prev, data])
-//     }
-//   }
-// );
-
 const ClientToken: React.FC<ClientTokenProps> = ({ range, clientsByIp }) => {
-  // const { loading, data: requestActivity, error: onFeedError } =
-  //   useRequestedCsTokenSubscription();
-  const REQUESTED_TOKEN: TypedDocumentNode<
-    RequestedCsTokenSubscription, RequestedCsTokenSubscriptionVariables
-  > = gql`
-subscription RequestedCSToken {
-  requestCS_Created {
-    sourceIp
-    originalIp
-    parentIp
-    relayed
-    requestedAt
-  }
-}
-`;
+  const REQUESTED_TOKEN: TypedDocumentNode<RequestedCsTokenSubscription, RequestedCsTokenSubscriptionVariables> = gql`
+      subscription RequestedCSToken {
+        requestCS_Created {
+          sourceIp
+          originalIp
+          parentIp
+          relayed
+          requestedAt
+        }
+      }
+    `;
+
+  const ACQUIRED_TOKEN: TypedDocumentNode<AcquiredCsTokenSubscription, AcquiredCsTokenSubscriptionVariables> = gql`
+      subscription AcquiredCSToken {
+        acquireCS_Created {
+          ip
+          sourceIp
+          acquiredAt
+        }
+      }
+    `;
 
   const [clientActions, setClientActions] = useState<ActionByIp>(clientsByIp);
-  const [lastActivity, setLastActivity] = useState<TokenAction|undefined>(undefined);
+  const [lastActivity, setLastActivity] = useState<TokenAction | undefined>(undefined);
 
   useSubscription(
     REQUESTED_TOKEN, {
@@ -61,18 +63,24 @@ subscription RequestedCSToken {
             originalIp: data.data.requestCS_Created.originalIp,
             action: data.data.requestCS_Created as RequestCs
           } as TokenAction
-        } 
+        }
       });
 
       setClientActions((state) => {
-
         if (data.data && data.data?.requestCS_Created && data.data.requestCS_Created.sourceIp) {
+
+          let clientForActivityIP: string = "";
+          if (data.data.requestCS_Created.originalIp === data.data.requestCS_Created.sourceIp) {
+            clientForActivityIP = data.data.requestCS_Created.sourceIp;
+          } else {
+            clientForActivityIP = data.data.requestCS_Created.originalIp;
+          }
 
           const newState = {
             ..._.cloneDeep(state),
-            [data.data.requestCS_Created.sourceIp]: {
-              client: { ..._.cloneDeep(state[data.data.requestCS_Created.sourceIp].client as Client) },
-              actions: [..._.cloneDeep(state[data.data.requestCS_Created.sourceIp].actions),
+            [clientForActivityIP]: {
+              client: { ..._.cloneDeep(state[clientForActivityIP].client as Client) },
+              actions: [..._.cloneDeep(state[clientForActivityIP].actions),
               {
                 parentIp: data.data.requestCS_Created.parentIp,
                 timestamp: data.data.requestCS_Created.requestedAt,
@@ -89,55 +97,59 @@ subscription RequestedCSToken {
 
           return { ...state };
         }
-
       });
     }
   });
 
+  useSubscription(
+    ACQUIRED_TOKEN, {
+    onData({ data }) {
 
+      setLastActivity(() => {
+        if (data.data && data.data?.acquireCS_Created && data.data.acquireCS_Created.ip) {
+          return {
+            parentIp: data.data.acquireCS_Created.sourceIp,
+            timestamp: data.data.acquireCS_Created.acquiredAt,
+            originalIp: data.data.acquireCS_Created.ip,
+            action: data.data.acquireCS_Created as AcquireCs
+          } as TokenAction
+        }
+      });
 
-  // useEffect(() => {
-  //   if (loading) { }
-  //   else if (onFeedError) {
-  //     console.log(`OnFeedError: ${JSON.stringify(onFeedError)}`);
+      setClientActions((state) => {
+        if (data.data && data.data?.acquireCS_Created && data.data.acquireCS_Created.ip) {
 
-  //   } else {
-  //     if (requestActivity && requestActivity.requestCS_Created) {
+          let clientForActivityIP: string = data.data.acquireCS_Created.ip;
 
-  //       setClientActions((state) => {
+          const newState = {
+            ..._.cloneDeep(state),
+            [clientForActivityIP]: {
+              client: { ..._.cloneDeep(state[clientForActivityIP].client as Client) },
+              actions: [..._.cloneDeep(state[clientForActivityIP].actions),
+              {
+                parentIp: data.data.acquireCS_Created.sourceIp,
+                timestamp: data.data.acquireCS_Created.acquiredAt,
+                originalIp: data.data.acquireCS_Created.ip,
+                action: data.data.acquireCS_Created as AcquireCs
+              } as TokenAction
+              ].slice(-5)
+            }
+          } as ActionByIp;
 
-  //         if (requestActivity && requestActivity.requestCS_Created && requestActivity.requestCS_Created.sourceIp) {
+          return newState;
 
-  //           const newState = {
-  //             ..._.cloneDeep(state),
-  //             [requestActivity.requestCS_Created.sourceIp]: {
-  //               client: { ..._.cloneDeep(state[requestActivity.requestCS_Created.sourceIp].client as Client) },
-  //               actions: [..._.cloneDeep(state[requestActivity.requestCS_Created.sourceIp].actions),
-  //               {
-  //                 parentIp: requestActivity.requestCS_Created.parentIp,
-  //                 timestamp: requestActivity.requestCS_Created.requestedAt,
-  //                 originalIp: requestActivity.requestCS_Created.originalIp,
-  //                 action: requestActivity.requestCS_Created as RequestCs
-  //               } as TokenAction
-  //               ].slice(-5)
-  //             }
-  //           } as ActionByIp;
+        } else {
 
-  //           return newState;
-
-  //         } else {
-
-  //           return { ...state };
-  //         }
-
-  //       });
-  //     }
-  //   }
-  // }, [loading, onFeedError, requestActivity]);
-
+          return { ...state };
+        }
+      });
+    }
+  });
 
   const clientsList = Object.entries(clientActions).map(([ip, action]) => {
     const activity = action.actions.map((activity, index) => {
+
+      // Highlight last activity in the table of client token activity.
       let highlighted: string = "";
       if (lastActivity && (lastActivity?.timestamp === activity.timestamp)) {
         highlighted = styles.highlightedItem;
@@ -145,8 +157,22 @@ subscription RequestedCSToken {
         highlighted = styles.unhighlightedItem;
       }
 
+      let backgroundItem: string = "";
+      switch (activity.action.__typename) {
+        case 'RequestCS':
+          if (activity.action.sourceIp !== ip) {
+            backgroundItem = styles.relayedItem;
+          } else {
+            backgroundItem = styles.requestedItem;
+          }
+          break;
+        default:
+          backgroundItem = styles.acquiredItem;
+          break;
+      }
+
       return (
-        <div key={`${index}${ip}`} className={`${highlighted} ${styles.activityItem}`}>
+        <div key={`${index}${ip}`} className={`${backgroundItem} ${highlighted} ${styles.activityItem}`}>
           <div className="ml-2 is-size-7">
             <label className="has-background-info-light px-1 ">Time stamp<br /></label>
             {`${format(parseISO(activity.timestamp), 'P hh:mm:ss:SSS ')}`}
@@ -154,6 +180,10 @@ subscription RequestedCSToken {
           <div className="ml-2 mt-2 is-size-7">
             <label className="has-background-info-light px-1">Parent<br /></label>
             {activity.parentIp}
+          </div>
+          <div className="ml-2 mt-2 is-size-7">
+            <label className="has-background-info-light px-1">Source IP<br /></label>
+            {activity.action.sourceIp}
           </div>
         </div>
       )
