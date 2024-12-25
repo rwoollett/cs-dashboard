153ea650dc90b9e25d62bac8172b1f4b9f3ca31d
Rodney Woollett <rodney.woollett@gmail.com>
2024-12-25 13:09:10 +1300

    GOL generation displayed in the canvas

--- front-end/src/components/CanvasGOL.tsx
+++ front-end/src/components/CanvasGOL.tsx
@@ -5,44 +5,119 @@ const createBoard = (rows: number, cols: number, initialValue: boolean): boolean
   for (let i = 0; i < rows; i++) {
     const row: boolean[] = [];
     for (let j = 0; j < cols; j++) {
+
       row.push(initialValue);
     }
     board.push(row);
   }
+
+
   return board;
 };
 
 
 const CanvasComponent: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const BLOCK_SIZE = 10;
-  const NSIZE = 35; //30; // number of rows/columns
+  const BLOCK_SIZE = 2;
+  const NSIZE = 380; //30; // number of rows/columns
   const WIDTH = BLOCK_SIZE * NSIZE;
-  const HEIGHT = WIDTH;
-  const board: Boolean[][] = createBoard(NSIZE, NSIZE, true);
-  const duration = 200;
+  const HEIGHT = WIDTH / 2;
+  const ALIVE = true;
+  //const DEAD = false;
+
+  const board: Boolean[][] = createBoard(NSIZE, NSIZE, false);
+  const duration = 1000;
+  const genDuration = 1000;
 
   const getRandomInt = (min: number, max: number): number => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
 
-  const paint = useCallback((ctx: CanvasRenderingContext2D) => {
-    const size = BLOCK_SIZE;
-    ctx.save();
+  const initBoard = () => {
     for (let i = 0; i < NSIZE; i++) {
       for (let j = 0; j < NSIZE; j++) {
-        if (board[i][j]) {
-          const x = i * size;
-          const y = j * size;
-          const r = getRandomInt(0, 255);
-          const g = getRandomInt(0, 255);
-          const b = getRandomInt(0, 255);
-          ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
-          ctx.fillRect(x, y, size, size);
+        const r = getRandomInt(0, 1000);
+        if (r < 180) {
+          board[i][j] = ALIVE;
         }
       }
     }
-    ctx.restore();
+
+  };
+
+  initBoard();
+
+  // Update GOL board on set inteval callback
+  const neighbourCount = useCallback((x: number, y: number): number => {
+    const OFFSETS: { x: number; y: number }[] = [
+      { x: -1, y: -1 },
+      { x: 0, y: -1 },
+      { x: 1, y: -1 },
+      { x: -1, y: 0 },
+      { x: 1, y: 0 },
+      { x: -1, y: 1 },
+      { x: 0, y: 1 },
+      { x: 1, y: 1 }
+    ];
+
+    return OFFSETS.reduce((prev, curr, i, arr) => {
+      const xp = x + curr.x;
+      const yp = y + curr.y;
+
+      if (!(xp < 0 || yp < 0 || xp >= NSIZE || yp >= NSIZE)) {
+        if (board[xp][yp] === ALIVE)
+          prev++;
+      }
+
+      return prev;
+    }, 0);
+  }, [ALIVE, board]);
+
+  const updateBoard = useCallback(() => {
+    const toggleCells: { x: number; y: number }[] = [];
+
+    for (let i = 0; i < NSIZE; i++) {
+      for (let j = 0; j < NSIZE; j++) {
+        let n = neighbourCount(i, j);
+        if (board[i][j] === ALIVE) {
+          if (n < 2)
+            toggleCells.push({ x: i, y: j });
+          if (n > 3)
+            toggleCells.push({ x: i, y: j });
+        } else {
+          if (n === 3)
+            toggleCells.push({ x: i, y: j });
+        }
+      }
+    }
+
+    for (const { x, y } of toggleCells) {
+      board[x][y] = !board[x][y];
+    }
+  }, [neighbourCount, ALIVE, board]);
+
+
+  const paint = useCallback((ctx: CanvasRenderingContext2D) => {
+    const size = BLOCK_SIZE;
+    const ALIVE_COLOR = 'rgb(0, 255, 0)'; // Green for alive cells
+    const DEAD_COLOR = 'rgb(255, 255, 255)'; // White for dead cells
+   // ctx.save();
+    for (let i = 0; i < NSIZE; i++) {
+      for (let j = 0; j < NSIZE; j++) {
+        const x = i * size;
+        const y = j * size;
+        if (board[i][j] === ALIVE) {
+          const r = 0; //getRandomInt(0, 255);
+          const g = 255; //getRandomInt(0, 255);
+          const b = 0;//getRandomInt(0, 255);
+          ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
+        } else {
+          ctx.fillStyle = DEAD_COLOR;
+        }
+        ctx.fillRect(x, y, size, size);
+      }
+    }
+    //ctx.restore();
   }, [board]);
 
   const draw = useCallback((ctx: CanvasRenderingContext2D) => {
@@ -60,16 +135,17 @@ const CanvasComponent: React.FC = () => {
         ctx.fillRect(0, 0, WIDTH, HEIGHT);
         ctx.beginPath();
         setInterval(() => draw(ctx), duration);
+        setInterval(() => updateBoard(), genDuration);
       }
     }
-  }, [HEIGHT, WIDTH, draw]);
+  }, [HEIGHT, WIDTH, draw, updateBoard]);
 
   return (
     <div className="panel">
       <p className="panel-heading mb-4">Game of Life</p>
       <div className="columns is-centered">
         <div className="column is-half">
-          <canvas
+          <canvas onClick={initBoard}
             ref={canvasRef}
             width={WIDTH}
             height={HEIGHT}
