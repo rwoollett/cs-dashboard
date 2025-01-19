de82a9b0c9dfbc5a5f6239cd55535d7c9fb0b153
Rodney Woollett <rodney.woollett@gmail.com>
2025-01-19 17:05:18 +1300

    Made the tic tac toe user page work. Has selection of X or O and hover of where they will place the move.

--- front-end/src/components/CanvasTTT.tsx
+++ front-end/src/components/CanvasTTT.tsx
@@ -1,61 +1,89 @@
-import { gql, TypedDocumentNode, useSubscription } from '@apollo/client';
-import React, { useRef, useState, useEffect, FormEvent, ChangeEvent } from 'react';
-import { BoardGenerationSubscription, BoardGenerationSubscriptionVariables } from '../graphql/generated/graphql-gol';
-import _ from 'lodash';
+import React, { useRef, useState, useEffect, FormEvent, ChangeEvent, MouseEvent } from 'react';
 import Dropdown, { Option } from './Dropdown';
-
+//import { gql, TypedDocumentNode } from '@apollo/client';
+//import _ from 'lodash';
 
 const CanvasComponent: React.FC = () => {
-  const [rowSize, setRowSize] = useState(30);
-  const [colSize, setColSize] = useState(40);
-  const [blockSize, setBlockSize] = useState(10)
-  const [genBoard, setGenBoard] = useState<number[][]>(() => {
-    const initArray: number[][] = Array.from({ length: 30 }, () => Array(40).fill(1));
-    return initArray;
-  });
-  const [gameId, setGameId] = useState(0);
-  const [player, setPlayer] = useState("X");
-  const [selection, setSelection] = useState<Option | null>(null);
-  const [opponentStart, setOpponentStart] = useState(true);
-
-  const options = [
-    { label: 'X (Cross)', value: 'X' },
-    { label: 'O (Nought)', value: 'O' },
+  const rowSize = 3;
+  const colSize = 3;
+  const blockSize = 80;
+  const board: number[] = Array(9).fill(0);
+  // const [genBoard, setGenBoard] = useState<number[]>(() => {
+  //   const initArray: number[] = Array(3).fill(0);
+  //   return initArray;
+  // });
+  board[0] = 2;
+  board[4] = 1;
+  board[8] = 2;
+  const playerCharactors = [
+    { label: 'X (Cross)', value: '1' },
+    { label: 'O (Nought)', value: '2' },
   ];
+  //const [gameId, setGameId] = useState(0);
+  const gameId = 0;
+  const [player, setPlayer] = useState<Option>(playerCharactors[0]);
+  const [isOpponentStart, setIsOpponentStart] = useState(true);
+  const [playerMove, setPlayerMove] = useState<number>(-1);
+  const [playerHover, setPlayerHover] = useState<number>(-1);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setPlayer(event.target.value);
-  };
-
-  const handlePlayerChange = (event: ChangeEvent<HTMLSelectElement>) => {
-    console.log('select value', event.target.value);
-    setPlayer(event.target.value);
-  };
-
-  const handleSelect = (newOption: Option) => {
+  const handlePlayerSelect = (newOption: Option) => {
     console.log(newOption.label, ' ', newOption.value);
-    setSelection(newOption);
+    setPlayer(newOption);
+  };
+
+  const handleOpponentStart = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value);
+    setIsOpponentStart(!isOpponentStart);
   };
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    //onSubmit();
-    //editBookById(book.id, title);
+    // Used to call useMuataion newGame...
   };
 
-
-  const UPDATE_GAME: TypedDocumentNode<BoardGenerationSubscription, BoardGenerationSubscriptionVariables> = gql`
-        subscription BoardGeneration {
-          board_Generation {
-            genId
-            rows
-            cols
-            board
-          }
+  const boardTraverse = (rect: DOMRect, x: number, y: number, dispatch: React.Dispatch<React.SetStateAction<number>>) => {
+    for (let i = 0; i < rowSize; i++) {
+      for (let j = 0; j < colSize; j++) {
+        const cy = i * blockSize;
+        const cx = j * blockSize;
+        const k = (i * rowSize) + j;
+        if (x > cx && x < (cx + blockSize) &&
+          y > cy && y < (cy + blockSize)) {
+          dispatch(k);
         }
-      `;
+      }
+    }
+  };
 
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const handleOnMouseMove = (event: MouseEvent<HTMLCanvasElement>) => {
+    const rect = event.currentTarget.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    boardTraverse(rect, x, y, setPlayerHover);
+  };
+
+  const handleOnMouseLeave = (event: MouseEvent<HTMLCanvasElement>) => {
+    setPlayerHover(-1);
+  };
+
+  const handleOnMouseDown = (event: MouseEvent<HTMLCanvasElement>) => {
+    const rect = event.currentTarget.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    boardTraverse(rect, x, y, setPlayerMove);
+  };
+
+  // const UPDATE_GAME: TypedDocumentNode<BoardGenerationSubscription, BoardGenerationSubscriptionVariables> = gql`
+  //       subscription BoardGeneration {
+  //         board_Generation {
+  //           genId
+  //           rows
+  //           cols
+  //           board
+  //         }
+  //       }
+  //     `;
 
   // useSubscription(
   //   UPDATE_GAME, {
@@ -73,41 +101,82 @@ const CanvasComponent: React.FC = () => {
   // });
 
   useEffect(() => {
-    const paint = (ctx: CanvasRenderingContext2D) => {
-      const size = blockSize;
-      const ALIVE = 1;
-      const ALIVE_COLORS: string[] = [
-        'rgb(255, 255, 255)', // White for dead cells
-        'rgb(0, 255, 0)',     // 1 Black(Alive green  )
-        'rgb(255, 255, 0)',   // 2 Lemon
-        'rgb(255, 82, 4)',    // 3 Orange
-        'rgb(201, 208, 181)', // 4 Pear
-        'rgb(0, 255, 0)',     // 5 Lime
-        'rgb(167, 12, 28)',   // 6 Strawberry
-        'rgb(175, 195, 102)', // 7 Grape
-        'rgb(255, 136, 5)',   // 8 Manderine
-        'rgb(255, 5, 5)'      // 8 Apple
-      ];
-      //console.log('paint');
+    const GAME_COLORS: string[] = [
+      'rgb(255, 255, 255)', // White for dead cells
+      'rgb(0, 0, 0)',       // 1 Black
+      'rgb(0, 255, 0)',     // 2 Green  
+      'rgb(255, 255, 0)',   // 3 Lemon
+      'rgb(255, 82, 4)',    // 3 Orange
+      'rgb(201, 208, 181)', // 4 Pear
+      'rgb(0, 255, 0)',     // 5 Lime
+      'rgb(167, 12, 28)',   // 6 Strawberry
+      'rgb(175, 195, 102)', // 7 Grape
+      'rgb(255, 136, 5)',   // 8 Manderine
+      'rgb(255, 5, 5)'      // 9 Apple
+    ];
 
-      ctx.fillStyle = '#FFFFFF';
-      ctx.clearRect(0, 0, colSize * size, rowSize * size);
-      //console.log('paint gen board', genBoard[0]);
-      const DEAD_COLOR = 'rgb(255, 255, 255)'; // White for dead cells
+    const drawPlayer = (ctx: CanvasRenderingContext2D, x: number, y: number, player: number) => {
+
+      const size = blockSize;
+      const centred = size / 2;
+      const radius = centred - 8;
+      switch (player) {
+        case 1:
+          // Cross
+          ctx.beginPath();
+          ctx.lineWidth = 4;
+          ctx.strokeStyle = GAME_COLORS[6];
+          ctx.moveTo(x + 18, y + 10);
+          ctx.lineTo(x + size - 18, y + size - 10);
+          ctx.moveTo(x - 18 + size, y + 10);
+          ctx.lineTo(x + 18, y + size - 10);
+          ctx.stroke();
+          break;
+
+        case 2:
+          // Only can be Nought as two options
+          ctx.beginPath();
+          ctx.lineWidth = 4;
+          ctx.strokeStyle = GAME_COLORS[6];
+          ctx.ellipse(x + centred, y + centred, radius - 3, radius, Math.PI, 0, 2 * Math.PI);
+          ctx.stroke();
+          break;
+
+        default:
+          // should not get here
+          break;
+      }
+    };
+    const paint = (ctx: CanvasRenderingContext2D) => {
+      const ALIVE = 1;
+      const BLANK_COLOR = GAME_COLORS[0]; // White for blank cells
+      const LINE_COLOR = GAME_COLORS[1]; // White for blank cells
+
+      ctx.fillStyle = LINE_COLOR; // The lines are dark coloured
+      ctx.clearRect(0, 0, colSize * blockSize, rowSize * blockSize);
+      ctx.fillRect(1, 1, (colSize * blockSize) - 2, (rowSize * blockSize) - 2);
+
       for (let i = 0; i < rowSize; i++) {
         for (let j = 0; j < colSize; j++) {
-          const y = i * size;
-          const x = j * size;
-          if (genBoard[i][j] >= ALIVE) {
-            // const r = 0; //getRandomInt(0, 255);
-            // const g = 255; //getRandomInt(0, 255);
-            // const b = 0;//getRandomInt(0, 255);
-            //ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
-            ctx.fillStyle = ALIVE_COLORS[genBoard[i][j]];
+          const y = i * blockSize;
+          const x = j * blockSize;
+          const k = (i * rowSize) + j;
+
+          ctx.fillStyle = BLANK_COLOR;
+          ctx.fillRect(x + 1, y + 1, blockSize - 2, blockSize - 2); // The cell background
+
+          if (board[k] >= ALIVE) {
+            drawPlayer(ctx, x, y, board[k]);
           } else {
-            ctx.fillStyle = DEAD_COLOR;
+            if (k === playerHover) {
+              const playerNumber = parseInt(player.value);
+              drawPlayer(ctx, x, y, playerNumber);
+            }
+            if (k === playerMove) {
+              const playerNumber = parseInt(player.value);
+              drawPlayer(ctx, x, y, playerNumber);
+            }
           }
-          ctx.fillRect(x, y, size, size);
         }
       }
     };
@@ -120,69 +189,44 @@ const CanvasComponent: React.FC = () => {
         return () => cancelAnimationFrame(animationFrameId);
       }
     }
-  }, [blockSize, rowSize, colSize, genBoard]);
+  }, [board, playerHover, playerMove, player]);
 
   return (
     <div className="panel">
       <p className="panel-heading mb-4">Tic Tac Toe {gameId}</p>
       <div className="columns">
         <div className="column">
-          <form onSubmit={handleSubmit}>
-            {/* <label className='label'>Player</label>
-            <input className='input' value={player} onChange={handleChange} />
- */}
-
-            <div className="field">
-              <label className="label">Player</label>
-              <Dropdown options={options} value={selection} onChange={handleSelect} />
-            </div>
-
-            {/* <div className="field">
-              <label className="label">Player</label>
-              <div className="control">
-                <div className="select is-normal is-multiple">
-                  <select size={4} value={player} onChange={handlePlayerChange}>
-                    <option>X</option>
-                    <option>0</option>
-                  </select>
+          <div className='panel'>
+            <p className="panel-heading mb-4 is-size-7">Select Game Options</p>
+            <div className='panel-block'>
+              <form onSubmit={handleSubmit}>
+                <div className="field">
+                  <label className="label">Play Charactor</label>
+                  <Dropdown options={playerCharactors} value={player} onChange={handlePlayerSelect} />
                 </div>
-              </div>
+                <div className="field">
+                  <div className="control">
+                    <label className="checkbox is-size-6">Opponent starts <input checked={isOpponentStart} onChange={handleOpponentStart} type="checkbox" className='is-size-6' /></label>
+                  </div>
+                </div>
+                <div className="field is-grouped">
+                  <div className="control">
+                    <button className="button is-link">Start Game</button>
+                  </div>
+                </div>
+              </form>
             </div>
-            <div className="field">
-              <label className="label">Player</label>
-              <div className="control">
-                <label className="radio">Play as
-                  <input type="radio" name="player" />
-                  X
-                </label>
-                <label className="radio"> or as O
-                  <input type="radio" name="player" />
-                </label>
-              </div>
-            </div> */}
-
-            <div className="field">
-              <div className="control">
-                <label className="checkbox">Opponent starts <input type="checkbox" /></label>
-              </div>
-            </div>
-
-            <div className="field is-grouped">
-              <div className="control">
-                <button className="button is-link">Start Game</button>
-              </div>
-              <div className="control">
-                <button className="button is-link is-light">Cancel</button>
-              </div>
-            </div>
-          </form>
+          </div>
         </div>
         <div className="column">
-          <canvas
+          <canvas className='is-clickable'
+            onMouseMove={handleOnMouseMove}
+            onMouseDown={handleOnMouseDown}
+            onMouseLeave={handleOnMouseLeave}
             ref={canvasRef}
             width={colSize * blockSize}
             height={rowSize * blockSize}
-            style={{ border: '1px solid #000000' }}>
+            style={{ border: '1px solid #EEEEEE' }}>
           </canvas>
         </div>
       </div>
