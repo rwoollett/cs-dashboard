29c0cbe031855e1263206166f54b85209b916648
Rodney Woollett <rodney.woollett@gmail.com>
2025-03-31 14:59:46 +1300

    add header for authorisation in ws link and http lins apollo client

--- front-end/src/client/apollo.ts
+++ front-end/src/client/apollo.ts
@@ -3,19 +3,46 @@ import {
   InMemoryCache, NormalizedCacheObject,
   from, split
 } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 import { onError } from '@apollo/client/link/error'
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 import { createClient } from 'graphql-ws';
 import { getMainDefinition } from '@apollo/client/utilities'
 
+const getToken = () => localStorage.getItem('jwtToken'); // Replace with your token retrieval logic
+
+// Add JWT token to HTTP headers using setContext
+const authLink = setContext((_, { headers }) => {
+  const token = getToken();
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : '', // Add the JWT token here
+    },
+  };
+});
+
 const wsLinkCSToken = new GraphQLWsLink(createClient({
   url: `${process.env.REACT_APP_CSTOKEN_APOLLO_SERVER_WS}/graphql`,
+  connectionParams: () => ({
+    headers: {
+      Authorization: `Bearer ${getToken()}`, // Add the JWT token here
+    },
+  }),
 }));
 
-const httpLinkCSToken = new HttpLink({
-  uri: `${process.env.REACT_APP_CSTOKEN_APOLLO_SERVER_URL}/graphql`, // Server URL (must be absolute)
-  credentials: 'same-origin', // Additional fetch() options like `credentials` or `headers`
-})
+// const httpLinkCSToken = new HttpLink({
+//   uri: `${process.env.REACT_APP_CSTOKEN_APOLLO_SERVER_URL}/graphql`, // Server URL (must be absolute)
+//   credentials: 'same-origin', // Additional fetch() options like `credentials` or `headers`
+// });
+const httpLinkCSToken = from([
+  authLink,
+  new HttpLink({
+    uri: `${process.env.REACT_APP_CSTOKEN_APOLLO_SERVER_URL}/graphql`, // Server URL (must be absolute)
+    credentials: 'same-origin', // Additional fetch() options like `credentials` or `headers`
+  }),
+]);
+
 
 const wsLinkGOL = new GraphQLWsLink(createClient({
   url: `${process.env.REACT_APP_GOL_APOLLO_SERVER_WS}/graphql`,
