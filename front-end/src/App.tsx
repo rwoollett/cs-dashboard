4c57cb1fb74d14bb495fda59c1675cd0e2210cd4
Rodney Woollett <rodney.woollett@gmail.com>
2025-07-20 11:55:02 +1200

    Apollo client not used

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,47 +1,21 @@
 import React from 'react';
 import MainPanel from './components/MainPanel';
-import { ApolloProvider } from '@apollo/client';
-import createApolloClient from './client/apollo';
 import { UsersProvider } from './context/users';
 import { WebSocketProvider } from "./context/websocket";
-import useUsersContext from './hooks/use-users-context';
 import Header from './components/Header';
-
-
-const ApolloClientProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const { getToken } = useUsersContext();
-  const [client, setClient] = React.useState<any>(null);
-
-  React.useEffect(() => {
-    const initializeClient = async () => {
-      const token = await getToken();
-      const apolloClient = createApolloClient(token); // Pass the token to the Apollo Client
-      setClient(apolloClient);
-    };
-    initializeClient();
-  }, [getToken]);
-
-  if (!client) {
-    return <div>Loading...</div>; // Render a loading state while the client is being initialized
-  }
-
-  return <ApolloProvider client={client}>{children}</ApolloProvider>;
-};
 
 const App: React.FC = () => {
 
   return (
     <UsersProvider>
-      <ApolloClientProvider>
-        <WebSocketProvider>
-          <section className="section">
-            <div className="pt-0 container">
-              <Header />
-              <MainPanel />
-            </div>
-          </section >
-        </WebSocketProvider>
-      </ApolloClientProvider>
+      <WebSocketProvider>
+        <section className="section">
+          <div className="pt-0 container">
+            <Header />
+            <MainPanel />
+          </div>
+        </section >
+      </WebSocketProvider>
     </UsersProvider>
 
   );
